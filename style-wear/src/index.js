6c35936f5e14ed5f55f1a666322de5eceff912ee
Devon Knight <devknight324@gmail.com>
2024-07-07 13:44:37 -0400

    removed uneccessary code

--- style-wear/src/index.js
+++ style-wear/src/index.js
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// takes all react components and renders them to the root element in DOM
+// renders the App component to the root element
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
