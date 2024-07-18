6f58a99e50527e6a129e82f99ae6f430b90dcb43
Devon Knight <devknight324@gmail.com>
2024-07-18 14:06:57 -0400

    index css organization

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 
 // import the App component from the App.js file
 import App from './App';
